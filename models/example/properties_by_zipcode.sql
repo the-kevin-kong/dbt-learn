8ab3504f487de9eb140072ce5e4b49fa46283f57
the-kevin-kong <kevin@bombas.com>
2019-02-08 10:09:37 -0500

    macro variable and jinja for loop

---
+++ models/example/properties_by_zipcode.sql
@@ -0,0 +1,20 @@
+{% set property_types = [
+    'house',
+    'apartment',
+    'townhouse'
+] %}
+
+select
+    zipcode,
+    {% for property_type in property_types -%}
+    sum(case when lower(property_type) = '{{property_type}}' then 1 else 0 end) as {{property_type}}_count
+
+    {{- "," if not loop.last }}
+
+    {% endfor %}
+
+from source_data.listings
+
+group by 1
+
+limit 100
